a6379b1fbd4e665e18496eb393ac216b98326e67
Timothy Cleveland <tjcp2@k12albemarle.org>
2017-02-01 19:19:44 -0500

    stuff...

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+ON_HEROKU = os.environ.get('ON_HEROKU')
 
-
+if ON_HEROKU:
+    # get the heroku port
+    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+else:
+    port = 3000
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
